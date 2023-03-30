6a0055ea1b65ae290100d7d862132f53b1be67d9
Elohina <elohinaguevara@gmail.com>
2023-03-30 09:00:43 +0200

    Change og image

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -47,7 +47,7 @@ const TemplateWrapper = ({ children }) => {
         <meta property='og:url' content='/' />
         <meta
           property='og:image'
-          content={`${withPrefix('/')}img/og-image.jpg`}
+          content={`${withPrefix('/')}img/quinzelogo.svg`}
         />
       </Helmet>
       <div>{children}</div>
